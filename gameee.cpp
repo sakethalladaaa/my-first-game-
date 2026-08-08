35ce03dde32455b9448adff173a697a0a6bc612e
sakethalladaaa <sakethalladaaa@gmail.com>
2026-08-09 01:57:56 +0530

    Updated game code

--- gameee.cpp
+++ gameee.cpp
@@ -1,5 +1,11 @@
 #include <raylib.h>
 
+enum class GameState {
+    MENU,
+    PLAYING,
+    GAME_OVER
+};
+
 class Board {
 private:
     char grid[3][3];
@@ -12,7 +18,7 @@ public:
     void reset() {
         for (int a = 0; a < 3; a++) {
             for (int b = 0; b < 3; b++) {
-                grid[a][b] = ' '; 
+                grid[a][b] = ' ';
             }
         }
     }
@@ -53,7 +59,7 @@ public:
         }
         return true;
     }
-}; 
+};
 
 int main(void) {
     InitWindow(600, 600, "Tic Tac Toe");
@@ -61,60 +67,90 @@ int main(void) {
 
     Board board;
     char currentTurn = 'X';
-    bool gameOver = false;
     char winner = ' ';
+    GameState currentState = GameState::MENU;
 
     while (!WindowShouldClose()) {
-        if (!gameOver && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            int col = GetMouseX() / 200;
-            int row = GetMouseY() / 200;
-            if (board.makeMove(row, col, currentTurn)) {
-                winner = board.checkWin();
-                if (winner != ' ' || board.isFull()) {
-                    gameOver = true;
-                } else {
-                    currentTurn = (currentTurn == 'X') ? 'O' : 'X';
+        switch (currentState) {
+            case GameState::MENU:
+                if (IsKeyPressed(KEY_ENTER) || IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    board.reset();
+                    currentTurn = 'X';
+                    winner = ' ';
+                    currentState = GameState::PLAYING;
                 }
-            }
-        }
+                break;
 
-        if (IsKeyPressed(KEY_R)) {
-            board.reset();
-            currentTurn = 'X';
-            gameOver = false;
-            winner = ' ';
+            case GameState::PLAYING:
+                if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    int col = GetMouseX() / 200;
+                    int row = GetMouseY() / 200;
+
+                    if (board.makeMove(row, col, currentTurn)) {
+                        winner = board.checkWin();
+                        if (winner != ' ' || board.isFull()) {
+                            currentState = GameState::GAME_OVER;
+                        } else {
+                            currentTurn = (currentTurn == 'X') ? 'O' : 'X';
+                        }
+                    }
+                }
+                break;
+
+            case GameState::GAME_OVER:
+                if (IsKeyPressed(KEY_R)) {
+                    board.reset();
+                    currentTurn = 'X';
+                    winner = ' ';
+                    currentState = GameState::PLAYING;
+                } else if (IsKeyPressed(KEY_M)) {
+                    currentState = GameState::MENU;
+                }
+                break;
         }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // 📏 Draw Grid Lines
-        DrawLine(200, 0, 200, 600, BLACK);
-        DrawLine(400, 0, 400, 600, BLACK);
-        DrawLine(0, 200, 600, 200, BLACK); 
-        DrawLine(0, 400, 600, 400, BLACK);
+        switch (currentState) {
+            case GameState::MENU:
+                DrawText("TIC TAC TOE", 130, 200, 50, DARKBLUE);
+                DrawText("Press ENTER or Click to Play", 120, 320, 24, DARKGRAY);
+                break;
 
-        for (int r = 0; r < 3; r++) {
-            for (int c = 0; c < 3; c++) {
-                char mark = board.getMark(r, c);
-                int centerX = c * 200 + 75;
-                int centerY = r * 200 + 50;
-                if (mark == 'X') {
-                    DrawText("X", centerX, centerY, 100, RED);
-                } else if (mark == 'O') {
-                    DrawText("O", centerX, centerY, 100, BLUE);
+            case GameState::PLAYING:
+                DrawLine(200, 0, 200, 600, BLACK);
+                DrawLine(400, 0, 400, 600, BLACK);
+                DrawLine(0, 200, 600, 200, BLACK);
+                DrawLine(0, 400, 600, 400, BLACK);
+
+                for (int r = 0; r < 3; r++) {
+                    for (int c = 0; c < 3; c++) {
+                        char mark = board.getMark(r, c);
+                        int centerX = c * 200 + 75;
+                        int centerY = r * 200 + 50;
+
+                        if (mark == 'X') {
+                            DrawText("X", centerX, centerY, 100, RED);
+                        } else if (mark == 'O') {
+                            DrawText("O", centerX, centerY, 100, BLUE);
+                        }
+                    }
                 }
-            }
-        }
+                break;
 
-        if (gameOver) {
-            DrawRectangle(50, 220, 500, 160, Fade(DARKGRAY, 0.9f));
-            if (winner != ' ') {
-                DrawText(TextFormat("Player %c Wins!", winner), 140, 250, 40, GOLD);
-            } else {
-                DrawText("It's a Draw!", 180, 250, 40, LIGHTGRAY);
-            }
-            DrawText("Press 'R' to Restart", 170, 310, 24, RAYWHITE);
+            case GameState::GAME_OVER:
+                DrawRectangle(50, 180, 500, 240, Fade(DARKGRAY, 0.95f));
+                
+                if (winner != ' ') {
+                    DrawText(TextFormat("Player %c Wins!", winner), 140, 220, 40, GOLD);
+                } else {
+                    DrawText("It's a Draw!", 180, 220, 40, LIGHTGRAY);
+                }
+
+                DrawText("Press 'R' to Restart", 170, 300, 24, RAYWHITE);
+                DrawText("Press 'M' for Main Menu", 160, 340, 24, LIGHTGRAY);
+                break;
         }
 
         EndDrawing();
