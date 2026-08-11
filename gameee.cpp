9c93c36a6083412822b822fc784da03f7c2c92e9
sakethalladaaa <sakethalladaaa@gmail.com>
2026-08-11 20:05:09 +0530

    XO GAME UPDATED VERSION

--- gameee.cpp
+++ gameee.cpp
@@ -1,142 +1,143 @@
 #include <raylib.h>
+#include <raymath.h>
 
-enum class GameState {MENU,PLAYING,
-   GAME_OVER};
+const Color A = { 20, 24, 33, 255 };
+const Color B = { 28, 34, 46, 255 };
+const Color C = { 36, 44, 60, 255 };
+const Color D = { 50, 62, 84, 255 };
+const Color E = { 255, 85, 115, 255 };
+const Color F = { 78, 205, 196, 255 };
+const Color G = { 255, 209, 102, 255 };
+const Color H = { 140, 152, 172, 255 };
 
-class Board{
-private:char grid[3][3];
-public:Board(){reset();}
+const int WW = 600;
+const int WH = 700;
+const int HH = 100;
+const int BS = 600;
+const int CS = BS / 3;
+const float CM = 12.0f;
 
-void reset(){
-for(int a=0;a<3;a++)
-    { for (int b = 0; b < 3; b++){
-grid[a][b] = ' ';
+enum class GS { M, P, GO };
+enum class WT { N, R, CO, DM, DA };
+
+struct WS {
+    char w = ' ';
+    WT t = WT::N;
+    int i = -1;
+};
+
+class Bd {
+private:
+    char g[3][3];
+public:
+    Bd() { rst(); }
+    void rst() { for(int i=0;i<3;i++) for(int j=0;j<3;j++) g[i][j]=' '; }
+    bool mv(int r, int c, char m) {
+        if(r>=0&&r<3&&c>=0&&c<3&&g[r][c]==' ') { g[r][c]=m; return true; }
+        return false;
     }
-   }
+    char gm(int r, int c) const { return g[r][c]; }
+    WS cw() const {
+        for(int i=0;i<3;i++) {
+            if(g[i][0]!=' '&&g[i][0]==g[i][1]&&g[i][1]==g[i][2]) return {g[i][0], WT::R, i};
+            if(g[0][i]!=' '&&g[0][i]==g[1][i]&&g[1][i]==g[2][i]) return {g[0][i], WT::CO, i};
+        }
+        if(g[0][0]!=' '&&g[0][0]==g[1][1]&&g[1][1]==g[2][2]) return {g[0][0], WT::DM, 0};
+        if(g[0][2]!=' '&&g[0][2]==g[1][1]&&g[1][1]==g[2][0]) return {g[0][2], WT::DA, 0};
+        return {' ', WT::N, -1};
+    }
+    bool ful() const {
+        for(int i=0;i<3;i++) for(int j=0;j<3;j++) if(g[i][j]==' ') return false;
+        return true;
+    }
+};
+
+void DX(Vector2 z, float s, float t, Color col) {
+    float h = s/2.0f;
+    DrawLineEx({z.x-h, z.y-h}, {z.x+h, z.y+h}, t, col);
+    DrawLineEx({z.x-h, z.y+h}, {z.x+h, z.y-h}, t, col);
 }
 
-  bool makeMove(int row,int col,char mark){
-    if(row >= 0&&row<3&&col>=0&&col<3&&grid[row][col]==' '){
- grid[row][col]=mark;
- return true;
-      }
- return false;}
-
-  char getMark( int row , int col ) const
-  {return grid[row][col];}
-
-  char checkWin() const {
-for(int i=0;i<3;i++){
-if(grid[i][0]!=' '&&grid[i][0]==grid[i][1]&&grid[i][1]==grid[i][2])
-return grid[i][0];
- if (grid[0][i] != ' ' && grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i])
-     return grid[0][i];}
- if(grid[0][0]!=' '&&grid[0][0]==grid[1][1]&&grid[1][1]==grid[2][2]) return grid[0][0];
-if(grid[0][2]!=' '&&grid[0][2]==grid[1][1]&&grid[1][1]==grid[2][0]) return grid[0][2];
- return ' ';
+void DO(Vector2 z, float rad, float t, Color col) {
+    DrawRing(z, rad - t/2.0f, rad + t/2.0f, 0, 360, 36, col);
 }
 
- bool isFull() const {
-  for (int r = 0; r < 3; r++) {for (int c = 0; c < 3; c++) {
-    if (grid[r][c] == ' ')
-                  return false;
-             }
-      }
-          return true;
-}
-  };
-
-int main( void ) {
-InitWindow ( 600 , 600 , "Tic Tac Toe" ) ;
-  SetTargetFPS(60);
-
-   Board board;
-  char currentTurn='X';char winner = ' ';
- GameState currentState=GameState::MENU;
-
-  while(!WindowShouldClose()){
-    switch(currentState){
-case GameState::MENU:
- if(IsKeyPressed(KEY_ENTER)||IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-        board.reset();
-  currentTurn = 'X';
-      winner=' ';
-             currentState=GameState::PLAYING;
-  }
-        break;
-
-      case GameState::PLAYING:
-if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-   int col=GetMouseX()/200;
-  int row = GetMouseY() / 200;
-
- if(board.makeMove(row,col,currentTurn)){
-         winner=board.checkWin();
-   if(winner!=' '||board.isFull()){
-        currentState = GameState::GAME_OVER;
-  } else {
- currentTurn=(currentTurn=='X')?'O':'X';
-      }
-   }
-}
-      break;
-
-case GameState::GAME_OVER:
-     if (IsKeyPressed(KEY_R)){
-        board.reset();currentTurn='X';winner=' ';
-    currentState = GameState::PLAYING;
-  }else if (IsKeyPressed(KEY_M)){
-currentState = GameState::MENU;
- }
-   break;
-   }
-
-BeginDrawing();
-  ClearBackground(RAYWHITE);
-
-switch(currentState){
-  case GameState::MENU:
-   DrawText("TIC TAC TOE", 130, 200, 50, DARKBLUE);
-      DrawText("Press ENTER or Click to Play", 120, 320, 24, DARKGRAY);
- break;
-
-  case GameState::PLAYING:
-      DrawLine(200,0,200,600,BLACK);DrawLine(400,0,400,600,BLACK);
-   DrawLine(0,200,600,200,BLACK);
-  DrawLine(0,400,600,400,BLACK);
-
-   for(int r=0;r<3;r++){
-   for(int c=0;c<3;c++){
-     char mark=board.getMark(r,c);
-       int centerX = c * 200 + 75;
-      int centerY = r * 200 + 50;
-
-if(mark=='X'){
-     DrawText("X", centerX, centerY, 100, RED);
-} else if (mark == 'O') {
-  DrawText("O", centerX, centerY, 100, BLUE);
-     }
-  }
-   }
-      break;
-
-  case GameState::GAME_OVER:
-  DrawRectangle(50,180,500,240,Fade(DARKGRAY,0.95f));
-
-   if (winner != ' ') {
-      DrawText(TextFormat("Player %c Wins!", winner), 140, 220, 40, GOLD);
- }else{
-   DrawText("It's a Draw!", 180, 220, 40, LIGHTGRAY);
-  }
-
-DrawText("Press 'R' to Restart",170,300,24,RAYWHITE);
-     DrawText("Press 'M' for Main Menu", 160, 340, 24, LIGHTGRAY);
- break;
+void DW(WS ws) {
+    if(ws.t == WT::N) return;
+    Vector2 s = {0}, e = {0}; float th = 10.0f;
+    if(ws.t == WT::R) { float y = HH + ws.i * CS + (CS/2.0f); s = {40.0f, y}; e = {(float)WW-40.0f, y}; }
+    else if(ws.t == WT::CO) { float x = ws.i * CS + (CS/2.0f); s = {x, (float)HH+40.0f}; e = {x, (float)WH-40.0f}; }
+    else if(ws.t == WT::DM) { s = {40.0f, (float)HH+40.0f}; e = {(float)WW-40.0f, (float)WH-40.0f}; }
+    else if(ws.t == WT::DA) { s = {(float)WW-40.0f, (float)HH+40.0f}; e = {40.0f, (float)WH-40.0f}; }
+    DrawLineEx(s, e, th, G);
 }
 
-      EndDrawing();
-}
-
- CloseWindow();
- return 0;
+int main() {
+    InitWindow(WW, WH, "Tic Tac Toe");
+    SetTargetFPS(60);
+    Bd b; char tu = 'X'; GS st = GS::M; WS ws = {' ', WT::N, -1};
+    while(!WindowShouldClose()) {
+        Vector2 mp = GetMousePosition();
+        switch(st) {
+            case GS::M:
+                if(IsKeyPressed(KEY_ENTER) || IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    b.rst(); tu = 'X'; ws = {' ', WT::N, -1}; st = GS::P;
+                }
+                break;
+            case GS::P:
+                if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && mp.y > HH) {
+                    int c = mp.x / CS; int r = (mp.y - HH) / CS;
+                    if(b.mv(r, c, tu)) {
+                        ws = b.cw();
+                        if(ws.w != ' ' || b.ful()) st = GS::GO;
+                        else tu = (tu == 'X') ? 'O' : 'X';
+                    }
+                }
+                break;
+            case GS::GO:
+                if(IsKeyPressed(KEY_R)) { b.rst(); tu = 'X'; ws = {' ', WT::N, -1}; st = GS::P; }
+                else if(IsKeyPressed(KEY_M)) { st = GS::M; }
+                break;
+        }
+        BeginDrawing();
+        ClearBackground(A);
+        switch(st) {
+            case GS::M: {
+                int tw = MeasureText("TIC TAC TOE", 48);
+                DrawText("TIC TAC TOE", (WW - tw)/2, 220, 48, E);
+                int sw = MeasureText("Press ENTER or Click anywhere to Play", 20);
+                DrawText("Press ENTER or Click anywhere to Play", (WW - sw)/2, 340, 20, H);
+                DX({220, 480}, 60, 10, E);
+                DO({380, 480}, 35, 10, F);
+                break;
+            }
+            case GS::P:
+            case GS::GO: {
+                DrawRectangle(0, 0, WW, HH, B);
+                if(st == GS::P) {
+                    DrawText(TextFormat("Turn: Player %c", tu), 30, 35, 32, (tu == 'X') ? E : F);
+                } else {
+                    if(ws.w != ' ') DrawText(TextFormat("Player %c Wins!", ws.w), 30, 35, 32, (ws.w == 'X') ? E : F);
+                    else DrawText("It's a Draw!", 30, 35, 32, H);
+                    DrawText("[R] Restart   [M] Menu", WW - 260, 42, 20, H);
+                }
+                for(int r=0; r<3; r++) {
+                    for(int c=0; c<3; c++) {
+                        Rectangle cr = {c*CS + CM, HH + r*CS + CM, CS - (CM*2), CS - (CM*2)};
+                        bool ih = CheckCollisionPointRec(mp, cr) && st == GS::P && b.gm(r, c) == ' ';
+                        DrawRectangleRounded(cr, 0.15f, 4, ih ? D : C);
+                        Vector2 cen = {cr.x + cr.width/2.0f, cr.y + cr.height/2.0f};
+                        char m = b.gm(r, c);
+                        if(m == 'X') DX(cen, 70.0f, 12.0f, E);
+                        else if(m == 'O') DO(cen, 40.0f, 12.0f, F);
+                    }
+                }
+                if(st == GS::GO && ws.w != ' ') DW(ws);
+                break;
+            }
+        }
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
 }
